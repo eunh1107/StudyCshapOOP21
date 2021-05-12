a5efbe70c442fda2cbaafefeacbe3194d36e2859
Eunheui Kim <dmsgml110712@gmail.com>
2021-05-12 09:07:25 +0900

    20210511_오후

---
+++ OOPSolution/TextEditorApp/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TextEditorApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            string filename = @"C:\Test\Help\PFRO.log"; //파일 Encoding필수
+            byte[] result;
+
+            using (FileStream stream = File.Open(filename, FileMode.Open))
+            {
+                result = new byte[stream.Length]; //파일크기만큼 byte[] 배열작성
+                await stream.ReadAsync(result, 0, (int)stream.Length);
+                //stream.Close(); //using이 Close() 자동으로 처리
+            }
+            richTextBox1.Text = Encoding.UTF8.GetString(result);
+        }
+    }
+}
